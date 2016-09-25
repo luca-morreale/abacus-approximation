3ed2a242016e371babe5d2344c44e872a17b2225
Luca Morreale <luca.morreale93@gmail.com>
2016-09-25 18:38:32 +0200

    Created graph struct

---
+++ src/include/graph.h
@@ -0,0 +1,18 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+
+#include "include/operations.h"
+#include "edges/operations.h"
+
+typedef struct {
+	OpList operations;
+	EdgeList edges;
+} Graph;
+
+
+
+
+
+
+#endif /* GRAPH_H */
