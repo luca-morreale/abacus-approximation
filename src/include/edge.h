70a6b280c9d95f493f85aed90067e4f679da672c
Luca Morreale <luca.morreale93@gmail.com>
2016-09-26 11:20:07 +0200

    Converted edge to cpp

--- src/include/edge.h
+++ src/include/edge.h
@@ -1,28 +1,11 @@
 #ifndef EDGE_H
 #define EDGE_H
 
-#include <string.h>
+#include <string>
+#include <map>
 
-
-typedef struct {
-	char key[10];
-	char value[20];
-} Edge;
-
-
-typedef struct EdgeList {
-	Edge *edge;
-	EdgeList *next;
-} EdgeList;
-
-
-Edge *newEdge(char *key, char *value);
-
-EdgeList *newEdgeList(Edge *edge);
-char *get(EdgeList *list, char *key);
-void append(EdgeList *list, char *key, char *value);
-void set(EdgeList *list, char *key, int value);
-void set(EdgeList *list, char *key, double value);
-void freeEdges(EdgeList *list);
+namespace graph {
+	typedef std::map<std::string, std::string> Edges;
+}
 
 #endif /* EDGE_H */
