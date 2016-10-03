6b9bd295b4e4121c5718d0195cc717d2e28f1572
Luca Morreale <luca.morreale93@gmail.com>
2016-10-03 14:24:19 +0200

    Added test case

--- src/test/test_execution.cpp
+++ src/test/test_execution.cpp
@@ -85,17 +85,17 @@ BOOST_AUTO_TEST_CASE(controlStructureExecution)
     executer::Executer exec;
     
     int out;
-    graph::NodePtr current = graph->next();
-    exec.runBasicOperation(current, graph);
-    current = graph->next();
-    exec.runBasicOperation(current, graph);
-    current = graph->next();
-    exec.runBasicOperation(current, graph);
+    graph::NodePtr current;
+    for (int i = 0; i < 3; i++) {       // run the three initial operation
+        current = graph->next();
+        exec.runBasicOperation(current, graph);
+    }
     
     current = graph->next();
     exec.runBasicOperation(current, graph);
     graph->get(current->out, out);
     BOOST_CHECK_EQUAL(0, out);
+    BOOST_CHECK_EQUAL("_if", current->out);
 
     exec.skipToEndBlock(current->out, graph);
     current = graph->next();
@@ -105,6 +105,7 @@ BOOST_AUTO_TEST_CASE(controlStructureExecution)
     BOOST_CHECK_EQUAL(0, out);
 
     current = graph->next();
+    BOOST_CHECK_EQUAL("_if", current->out);
     exec.runNode(current, graph);
     
 
@@ -117,3 +118,60 @@ BOOST_AUTO_TEST_CASE(controlStructureExecution)
 
     in.close();
 }
+
+
+BOOST_AUTO_TEST_CASE(loopStructureExecution)
+{
+    
+    ifstream in("/home/luca/projects/School/aapp/code/src/test/complex_benchmark.graph");
+    graph::GraphPtr graph = parser::extractGraph(in, "int");
+    executer::Executer exec;
+    
+    int out;
+    graph::NodePtr current;
+    for (int i = 0; i < 3; i++) {       // run the three initial operation
+        current = graph->next();
+        exec.runBasicOperation(current, graph);
+    }
+
+    for (int i = 0; i < 2; i++) {   // run the two if structure
+        current = graph->next();
+        exec.runNode(current, graph);
+
+
+        current = graph->next();
+        exec.runBasicOperation(current, graph);
+    }
+
+    // here we start check the loop
+
+    current = graph->next();
+    exec.runBasicOperation(current, graph);     // set counter to zero
+    current = graph->next();
+    exec.runBasicOperation(current, graph);     // set i to zero
+
+    current = graph->next();
+    BOOST_CHECK_EQUAL("_for", current->out);
+    exec.runNode(current, graph);
+
+    graph->get("i", out);
+    BOOST_CHECK_EQUAL(10, out);
+
+    graph->get("counter", out);
+    BOOST_CHECK_EQUAL(45, out);
+
+    /*current = graph->next();
+    BOOST_CHECK_EQUAL("_do", current->out);
+    exec.runNode(current, graph);
+
+    graph->get("i", out);
+    BOOST_CHECK_EQUAL(10, out);
+
+    graph->get("counter", out);
+    BOOST_CHECK_EQUAL(45, out);*/
+
+
+    in.close();
+}
+
+
