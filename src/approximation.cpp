83c7ce73c7f20c25f36db2c4556b81737be46b09
Luca Morreale <luca.morreale93@gmail.com>
2016-10-07 15:18:54 +0200

    Added functions to extract nodes from graph

--- src/approximation.cpp
+++ src/approximation.cpp
@@ -3,6 +3,74 @@
 
 namespace approximation {
 
+    std::vector<Approximation> approximations(approx_values, approx_values + map_start_values_size);
+
+    std::map<Approximation, std::string> approximationsName(map_start_values, map_start_values + map_start_values_size);
+
+    std::map<Approximation, std::string> approximationsOp(op_start_values, op_start_values + op_map_values_size);
+
+
+    graph::Nodes selectSuitableNodes(graph::GraphPtr graph, Approximation approximation)
+    {
+        if(approximation == approximateLoop) {
+            return selectIncrements(graph);
+        } else if(approximation == approximateValue) {
+            return selectAll(graph);
+        } else {
+            return selectOperation(graph, approximationsOp[approximation]);
+        }
+    }
+
+    graph::Nodes selectOperation(graph::GraphPtr graph, std::string op)
+    {
+        graph::Nodes suitableNodes;
+        graph::NodePtr current;
+        while ((current=graph->next()) != NULL) {
+            if (executer::is(current->op, op)) {
+                suitableNodes.push_back(current);
+            }
+        }
+        return suitableNodes;
+    }
+
+    graph::Nodes selectAll(graph::GraphPtr graph)
+    {
+        graph::Nodes suitableNodes;
+        graph::NodePtr current;
+        while ((current=graph->next()) != NULL) {
+            if (!syntax::Syntax::isControlOp(current->out)) {
+                suitableNodes.push_back(current);
+            }
+        }
+        return suitableNodes;
+    }
+
+    graph::Nodes selectIncrements(graph::GraphPtr graph)
+    {
+        graph::Nodes suitableNodes;
+        graph::NodePtr current;
+        while ((current=graph->next()) != NULL) {
+            if (executer::is(current->out, "_for") && executer::is(current->op, "")) {
+                suitableNodes.push_back(getIncrementNode(graph));
+                skipForwardToEnd(graph);
+            }
+        }
+        return suitableNodes;
+    }
+
+    graph::NodePtr getIncrementNode(graph::GraphPtr graph)
+    {
+        graph->rollback();
+        return graph->rollback();
+    }
+
+    void skipForwardToEnd(graph::GraphPtr graph)
+    {
+        graph->next();
+        graph->next();
+    }
+
+    
     graph::Nodes approximateSum(graph::NodePtr node)
     {
         graph::Nodes replacement;
@@ -16,24 +84,32 @@ namespace approximation {
         graph::Nodes replacement;
         replacement.push_back(graph::newNode(node));
         replacement.push_back(graph::newNode(node));
-        
-        replacement[0]->op = "~";
-        replacement[0]->out = "tmp";
-        replacement[0]->incoming[0] = replacement[0]->incoming[1];
 
-        replacement[1]->op = "&";
-        replacement[1]->incoming[1] = "tmp";
+        replaceOperations(replacement, "~", "&", replacement[0]->incoming[1]);
 
         return replacement;
     }
 
     graph::Nodes approximateMult(graph::NodePtr node)
     {
+        graph::Nodes replacement;
+        replacement.push_back(graph::newNode(node));
+        replacement.push_back(graph::newNode(node));
 
+        replaceOperations(replacement, "<<", "+", calculateShift());
+
+        return replacement;
     }
+    
     graph::Nodes approximateDiv(graph::NodePtr node)
     {
+        graph::Nodes replacement;
+        replacement.push_back(graph::newNode(node));
+        replacement.push_back(graph::newNode(node));
 
+        replaceOperations(replacement, ">>", "-", calculateShift());
+
+        return replacement;
     }
 
     graph::Nodes approximateLoop(graph::NodePtr node)
@@ -41,20 +117,43 @@ namespace approximation {
         graph::Nodes replacement;
         replacement.push_back(graph::newNode(node));
         
-        if(syntax::isNumber(replacement[0]->incoming[0])) {
-            int index = std::stoi(replacement[0]->incoming[0]) * 2;
-            replacement[0]->incoming[0] = index +"";
-        } else {
-            int index = std::stoi(replacement[0]->incoming[1]) * 2;
-            replacement[0]->incoming[1] = index +"";
-        }
+        doubleIncrement(replacement);
 
         return replacement;
     }
 
-    graph::Nodes approximateType(graph::NodePtr node)  //uhm
+    graph::Nodes approximateValue(graph::NodePtr node)  // put how many bit to zero?
     {
 
     }
+
+    void replaceOperations(graph::Nodes &replacement, std::string op1, std::string op2, std::string operand)
+    {
+        replacement[0]->op = op1;
+        replacement[0]->out = "tmp";
+        replacement[0]->incoming[1] = operand;
+
+        replacement[1]->op = op2;
+        replacement[1]->incoming[0] = "tmp";
+    }
+
+    std::string calculateShift()
+    {
+
+        return 0 + "";
+    }
+
+    void doubleIncrement(graph::Nodes &replacement)
+    {
+        if(syntax::isNumber(replacement[0]->incoming[0])) {
+            int index = std::stoi(replacement[0]->incoming[0]) * 2;
+            replacement[0]->incoming[0] = index + "";
+        } else {
+            int index = std::stoi(replacement[0]->incoming[1]) * 2;
+            replacement[0]->incoming[1] = index + "";
+        }
+    }
+
+    
 
 }
--- src/include/approximation.h
+++ src/include/approximation.h
@@ -1,21 +1,64 @@
 #ifndef APPROXIMATION_H
 #define APPROXIMATION_H
 
-#include <vector>
+#include <map>
 
 #include "node.h"
+#include "graph.h"
 #include "syntax.h"
+#include "executer.h"
 
 namespace approximation {
 
     typedef graph::Nodes (*Approximation)(graph::NodePtr node);
+
+    graph::Nodes selectSuitableNodes(graph::GraphPtr graph, Approximation approximation);
+    graph::Nodes selectOperation(graph::GraphPtr graph, std::string op);
+    graph::Nodes selectAll(graph::GraphPtr graph);
+    graph::Nodes selectIncrements(graph::GraphPtr graph);
+    graph::NodePtr getIncrementNode(graph::GraphPtr graph);
+    void skipForwardToEnd(graph::GraphPtr graph);
 
     graph::Nodes approximateSum(graph::NodePtr node);
     graph::Nodes approximateMinus(graph::NodePtr node);
     graph::Nodes approximateMult(graph::NodePtr node);
     graph::Nodes approximateDiv(graph::NodePtr node);
     graph::Nodes approximateLoop(graph::NodePtr node);
-    graph::Nodes approximateType(graph::NodePtr node);  //uhm
+    graph::Nodes approximateValue(graph::NodePtr node);
+
+    void replaceOperations(graph::Nodes &replacement, std::string op1, std::string op2, std::string operand);
+    std::string calculateShift();
+    void doubleIncrement(graph::Nodes &replacement);
+
+    extern std::vector<Approximation> approximations;
+    extern std::map<Approximation, std::string> approximationsName;
+
+    typedef std::pair<Approximation, std::string> approxpair;
+
+    const Approximation approx_values[] = {
+        &approximateSum, &approximateMinus, &approximateMult,
+        &approximateDiv, &approximateLoop, &approximateValue
+    };
+
+    const approxpair map_start_values[] = {
+        approxpair(&approximateSum, "sum"),
+        approxpair(&approximateMinus, "min"),
+        approxpair(&approximateMult, "mult"),
+        approxpair(&approximateDiv, "div"),
+        approxpair(&approximateLoop, "loop"),
+        approxpair(&approximateValue, "value")
+    };
+
+    const approxpair op_start_values[] = {
+        approxpair(&approximateSum, "+"),
+        approxpair(&approximateMinus, "-"),
+        approxpair(&approximateMult, "*"),
+        approxpair(&approximateDiv, "/")
+    };
+
+    const size_t map_start_values_size = 6;
+
+    const size_t op_map_values_size = 4;
 
 }
 
