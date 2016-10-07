3a81fb109baa1e36bcf2d23460a5422346defa2b
Luca Morreale <luca.morreale93@gmail.com>
2016-10-07 15:20:36 +0200

    Implemented functions for approximation

--- src/abacus.cpp
+++ src/abacus.cpp
@@ -1,4 +1,5 @@
 #include "include/abacus.h"
+#include <list>
 
 
 namespace abacus {
@@ -17,22 +18,25 @@ namespace abacus {
         AppGraphPtr original = new AppGraph(*graph);
         runGraph(graph);
         for(int i = 0; i < this->N; i++) {
-
-            std::vector<PairGraph> approximatedGraphs;
-
+            std::list<PairFitness> approximatedGraphs;
+            
+            #pragma omp parallel for default(shared) shared(approximatedGraphs) num_threads(8)
             for(int j = 0; j < this->M; j++) {
                 approximation::Approximation approximation = selectApproximation();
                 AppGraphPtr appGraph = approximate(original, approximation);
                 double accuracy = evalAccuracy(appGraph, graph);
                 if(accuracy < this->threshold) {
-                    double fitness = evaluateFitness(appGraph, accuracy);
-                    approximatedGraphs.push_back(std::make_pair(fitness, appGraph));
+                    double fitness = evaluateFitness(approximation, accuracy);
+                    #pragma omp critical
+                    {
+                        approximatedGraphs.push_back(make_pair(fitness, approximation, appGraph));
+                    }
                 }
             }
 
             if(approximatedGraphs.size() > 0) {
-                std::sort(approximatedGraphs.begin(), approximatedGraphs.end(), cmpPairs);
-                original = approximatedGraphs[0].second;
+                approximatedGraphs.sort(cmpPairs);
+                original = approximatedGraphs.front().second.second;
             }
         }
 
@@ -40,17 +44,60 @@ namespace abacus {
 
     approximation::Approximation ABACUSExecuter::selectApproximation()
     {   
-        
-        return 0;
+        srand(time(NULL));
+        int index = rand() % approximation::approximations.size();
+        return approximation::approximations[index];
     }
-    AppGraphPtr ABACUSExecuter::approximate(AppGraphPtr graph, approximation::Approximation approximation) {return NULL;}
-    double ABACUSExecuter::evalAccuracy(AppGraphPtr graph, graph::GraphPtr original) {return 0.0;}
-    double ABACUSExecuter::evaluateFitness(AppGraphPtr graph, double accuracy) {return 0.0;}
+
+    AppGraphPtr ABACUSExecuter::approximate(AppGraphPtr graph, approximation::Approximation approximation)
+    {
+        AppGraphPtr copy = new AppGraph(*graph);
+        graph::Nodes suitableNodes = approximation::selectSuitableNodes(copy, approximation);
+
+        graph::NodePtr node = selectRandomNode(suitableNodes, approximation);
+        copy->substitute(approximation(node), node);
+
+        return copy;
+    }
+
+    graph::NodePtr ABACUSExecuter::selectRandomNode(graph::Nodes suitableNodes, approximation::Approximation approximation)
+    {
+        srand(time(NULL));
+        int index = rand() % suitableNodes.size();
+        return suitableNodes[index];
+    }
+    
+    double ABACUSExecuter::evalAccuracy(AppGraphPtr graph, graph::GraphPtr original)
+    {
+        AppGraphPtr copy = new AppGraph(*graph);
+        this->runGraph(copy);
+
+        double accuracy = 0;
+        auto outputList = original->getOutputList();
+        for(auto it = outputList.begin(); it != outputList.end(); it++) {
+            double app, real;
+            original->get(*it, real);
+            copy->get(*it, app);
+            accuracy += real - app;
+        }
 
 
+        return accuracy;
+    }
+    
+    double ABACUSExecuter::evaluateFitness(approximation::Approximation approximation, double accuracy)
+    {
+        double precisionErr = (approximation == approximation::approximateValue)? 0.1 : 0;
+        return this->a1 * accuracy + this->a2 * precisionErr;
+    }
+
+    ABACUSExecuter::PairFitness ABACUSExecuter::make_pair(double fitness, approximation::Approximation approximation, AppGraphPtr graph)
+    {
+        return std::make_pair(fitness, std::make_pair(approximation, graph));
+    }
 
 
-    bool ABACUSExecuter::cmpPairs(PairGraph a, PairGraph b)
+    bool ABACUSExecuter::cmpPairs(PairFitness a, PairFitness b)
     {
         return a.first < b.first;
     }
--- src/include/abacus.h
+++ src/include/abacus.h
@@ -2,6 +2,7 @@
 #define ABACUS_H
 
 #include <algorithm>
+#include <omp.h>
 
 #include "graph.h"
 #include "executer.h"
@@ -13,32 +14,41 @@ namespace abacus {
     typedef approximation::ApproximatedGraph AppGraph;
     typedef approximation::ApproximatedGraphPtr AppGraphPtr;
     
-    class ABACUSExecuter : executer::Executer {
+    class ABACUSExecuter : public executer::Executer {
     public:
         ABACUSExecuter(int N, int M, double threshold);
         ~ABACUSExecuter();
 
-        void runAbacusOnGraph(graph::GraphPtr graph);
+        virtual void runAbacusOnGraph(graph::GraphPtr graph);
 
         int getN() { return N; }
         int getM() { return M; }
         double getThreshold() { return threshold; }
 
     protected:
-        typedef std::pair<double, approximation::ApproximatedGraphPtr> PairGraph;
-        static bool cmpPairs(PairGraph a, PairGraph b);
+        typedef std::pair<approximation::Approximation, AppGraphPtr> PairAppr;
+        typedef std::pair<double, PairAppr> PairFitness;
+
+        static bool cmpPairs(PairFitness a, PairFitness b);
 
         virtual approximation::Approximation selectApproximation();
         virtual AppGraphPtr approximate(AppGraphPtr graph, approximation::Approximation approximation);
         virtual double evalAccuracy(AppGraphPtr graph, graph::GraphPtr original);
-        virtual double evaluateFitness(AppGraphPtr graph, double accuracy);
+        virtual double evaluateFitness(approximation::Approximation approximation, double accuracy);
 
     private:
         int N;
         int M;
         double threshold;
 
+        const double a1 = 0.8;
+        const double a2 = 0.12;
+        //const double a3 = 0.08;
+
         ABACUSExecuter() :Executer() { }
+
+        graph::NodePtr selectRandomNode(graph::Nodes suitableNodes, approximation::Approximation approximation);
+        PairFitness make_pair(double fitness, approximation::Approximation approximation, AppGraphPtr graph);
 
 
     };
