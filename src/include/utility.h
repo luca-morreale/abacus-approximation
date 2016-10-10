9a64c5c7f99febeb1b33fa915e66ce6e197c5209
Luca Morreale <luca.morreale93@gmail.com>
2016-10-10 13:13:47 +0200

    Added extraction of values and keys of map

--- src/include/utility.h
+++ src/include/utility.h
@@ -2,7 +2,9 @@
 #define UTILITY_H
 
 #include <cstdlib>
+#include <map>
 #include <string>
+#include <vector>
 
 #include "node.h"
 
@@ -25,6 +27,26 @@ T selectUniformlyRandom(std::vector<T> array)
 {
     int index = rand() % array.size();
     return array[index];
+}
+
+template <typename T, typename E>
+std::vector<E> values(std::map<T, E> m)
+{
+    std::vector<E> v;
+    for(auto it = m.begin(); it != m.end(); ++it) {
+        v.push_back(it->second);
+    }
+    return v;
+}
+
+template <typename T, typename E>
+std::vector<T> keys(std::map<T, E> m)
+{
+    std::vector<T> v;
+    for(auto it = m.begin(); it != m.end(); ++it) {
+        v.push_back(it->first);
+    }
+    return v;
 }
 
 
