e692d255395feeff82c2999ce869995484d866f1
Luca Morreale <luca.morreale93@gmail.com>
2016-10-12 16:19:29 +0200

    Added loading of input data

--- src/main.cpp
+++ src/main.cpp
@@ -9,56 +9,81 @@
 #include "include/executer.h"
 #include "include/abacus.h"
 
-#define PARAMETERS 4
+#define PARAMETERS 6
 #define FLAG_FILE "-f"
+#define FLAG_INPUT "-i"
 #define FLAG_TYPE "-t"
 
 using namespace std;
 
 
-void extractFlags(int argc, char *argv[], string &benchmark, string &type)
+void assign(string &str, char source[])
+{
+    str.assign(source, strlen(source));
+}
+
+void extractFlags(int argc, char *argv[], string &benchmark, string &type, string &input)
 {
     for(int i = 1; i < argc; i++) {
         if(strcmp(FLAG_TYPE, argv[i]) == 0) {
-            type.assign(argv[i], strlen(argv[++i]));
+            assign(type, argv[i++]);
         } else if(strcmp(FLAG_FILE, argv[i]) == 0) {
-            benchmark.assign(argv[i], strlen(argv[++i]));
+            assign(benchmark, argv[i++]);
+        } else if(strcmp(FLAG_INPUT, argv[i]) == 0) {
+            assign(input, argv[i++]);
         }
     }
 }
 
+
+graph::GraphPtr loadFromFiles(string benchFile, string type, string inputFile)
+{
+    ifstream bench(benchFile);
+    if(!bench.is_open()) {
+        cout << "Could not open " << benchFile << ", path or file invalid" << std::endl;
+        exit(1);
+    }
+
+    graph::GraphPtr graph = parser::extractGraph(bench, type);
+    bench.close();
+
+    ifstream input(inputFile);
+    if(!input.is_open()) {
+        cout << "Couldn't open " << inputFile << ", path or file invalid" << std::endl;
+        exit(1);
+    }
+    graph::Edges edges = parser::extractEdges(input);
+    graph->loadInputData(edges);
+    input.close();
+
+    return graph;
+}
 
 int main(int argc, char *argv[])
 {
     srand(time(NULL));
     
     if(argc < PARAMETERS + 1) {
-        cout << "Usage: -f benchmark_filename -t data_type" << endl;
+        cout << "Usage: -f benchmark_filename -t data_type -i input_data_file" << endl;
         return 0;
     }
 
     string benchmark;
+    string input;
     string type;
-    extractFlags(argc, argv, benchmark, type);
+    extractFlags(argc, argv, benchmark, type, input);
 
-    ifstream cin(benchmark);
-    if(!cin.is_open()) {
-        cout << "Couldn't open " << benchmark << ", path or file invalid" << std::endl;
-        return 1;
-    }
-
-    graph::GraphPtr graph = parser::extractGraph(cin, type);
+    graph::GraphPtr graph = loadFromFiles(benchmark, type, input);
 
     #ifdef DEBUG
         graph->debug();
     #endif
 
-    executer::ExecuterPtr exec = new executer::Executer();
+    executer::Executer exec;
 
-    exec->runGraph(graph);
+    exec.runGraph(graph);
 
-    cin.close();
-
+    delete(graph);
 
     return 0;
 }
