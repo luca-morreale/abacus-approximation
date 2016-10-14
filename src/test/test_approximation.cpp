2b072d2be1f8a7dee46d72d75a2bea301449c32d
Luca Morreale <luca.morreale93@gmail.com>
2016-10-14 23:13:55 +0200

    Added test case on value approximation

--- src/test/test_approximation.cpp
+++ src/test/test_approximation.cpp
@@ -82,7 +82,7 @@ BOOST_AUTO_TEST_CASE(divisionApproximation)
     in.close();
 }
 
-BOOST_AUTO_TEST_CASE(multpproximation)
+BOOST_AUTO_TEST_CASE(multApproximation)
 {
     ifstream in("../../src/test/benchmark.graph");
     graph::GraphPtr original = parser::extractGraph(in, "int");
@@ -105,6 +105,35 @@ BOOST_AUTO_TEST_CASE(multpproximation)
     BOOST_CHECK_EQUAL(current->incoming[1], replacements[1]->incoming[1]);
     BOOST_CHECK_EQUAL("tmp", replacements[1]->incoming[0]);
     BOOST_CHECK_EQUAL("+", replacements[1]->op);
+    BOOST_CHECK_EQUAL(current->out, replacements[1]->out);
+    
+    delete(original);
+    in.close();
+}
+
+BOOST_AUTO_TEST_CASE(valueApproximation)
+{
+    ifstream in("../../src/test/benchmark.graph");
+    graph::GraphPtr original = parser::extractGraph(in, "int");
+
+    for(int i=0; i<3; i++) {
+        original->next();
+    }
+
+    graph::NodePtr current = original->next();
+    int mask = -1;
+
+    graph::Nodes replacements = approximation::approximateValue(current, mask);
+    BOOST_CHECK_PREDICATE(std::not_equal_to<int>(), (-1)(mask));
+    BOOST_CHECK_EQUAL(2, replacements.size());
+    BOOST_CHECK_EQUAL(current->incoming[0], replacements[0]->incoming[0]);
+    BOOST_CHECK_EQUAL(std::to_string(mask), replacements[0]->incoming[1]);
+    BOOST_CHECK_EQUAL("&", replacements[0]->op);
+    BOOST_CHECK_EQUAL("tmp", replacements[0]->out);
+
+    BOOST_CHECK_EQUAL(current->incoming[1], replacements[1]->incoming[1]);
+    BOOST_CHECK_EQUAL("tmp", replacements[1]->incoming[0]);
+    BOOST_CHECK_EQUAL(current->op, replacements[1]->op);
     BOOST_CHECK_EQUAL(current->out, replacements[1]->out);
     
     delete(original);
