6adf15b42ebf5e200e547cfdd8d012a954a2cbf0
Luca Morreale <luca.morreale93@gmail.com>
2016-10-14 23:25:57 +0200

    Added test case for simple selection of replaceable nodes

---
+++ src/test/test_selection.cpp
@@ -0,0 +1,71 @@
+#define BOOST_TEST_MODULE SelectionTests
+
+#include <boost/test/unit_test.hpp>
+#include <fstream>
+#include "approximation.h"
+#include "parser.h"
+
+using namespace std;
+
+
+BOOST_AUTO_TEST_CASE(sumSelection)
+{
+    ifstream in("../../src/test/complex_benchmark.graph");
+    graph::GraphPtr original = parser::extractGraph(in, "int");
+
+    auto nodes = approximation::selectSuitableNodes(original, approximation::approximateSum);
+    BOOST_CHECK_EQUAL(16, nodes.size());
+
+    
+
+    delete(original);
+    in.close();
+}
+
+BOOST_AUTO_TEST_CASE(subtractionSelection)
+{
+    ifstream in("../../src/test/complex_benchmark.graph");
+    graph::GraphPtr original = parser::extractGraph(in, "int");
+
+    auto nodes = approximation::selectSuitableNodes(original, approximation::approximateMinus);
+    BOOST_CHECK_EQUAL(2, nodes.size());
+    
+    delete(original);
+    in.close();
+}
+
+BOOST_AUTO_TEST_CASE(divisionSelection)
+{
+    ifstream in("../../src/test/complex_benchmark.graph");
+    graph::GraphPtr original = parser::extractGraph(in, "int");
+
+    auto nodes = approximation::selectSuitableNodes(original, approximation::approximateDiv);
+    BOOST_CHECK_EQUAL(1, nodes.size());
+    
+    delete(original);
+    in.close();
+}
+
+BOOST_AUTO_TEST_CASE(multSelection)
+{
+    ifstream in("../../src/test/complex_benchmark.graph");
+    graph::GraphPtr original = parser::extractGraph(in, "int");
+
+    auto nodes = approximation::selectSuitableNodes(original, approximation::approximateMult);
+    BOOST_CHECK_EQUAL(0, nodes.size());
+    
+    delete(original);
+    in.close();
+}
+
+BOOST_AUTO_TEST_CASE(valueSelection)
+{
+    ifstream in("../../src/test/complex_benchmark.graph");
+    graph::GraphPtr original = parser::extractGraph(in, "int");
+
+    auto nodes = approximation::selectSuitableNodes(original, approximation::approximateValue);
+    BOOST_CHECK_EQUAL(19, nodes.size());
+    
+    delete(original);
+    in.close();
+}
