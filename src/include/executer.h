aac01a99da538151757a8b1fd7a4dca1f78c4ef8
Luca Morreale <luca.morreale93@gmail.com>
2016-12-07 16:59:13 +0100

    Added virtual modifier

--- src/include/executer.h
+++ src/include/executer.h
@@ -17,37 +17,37 @@ namespace executer {
         Executer();
         ~Executer();
 
-        void runGraph(graph::GraphPtr graph);
+        virtual void runGraph(graph::GraphPtr graph);
 
-        void runNode(graph::NodePtr node, graph::GraphPtr graph);
-        void runBasicOperation(graph::NodePtr node, graph::GraphPtr graph);
+        virtual void runNode(graph::NodePtr node, graph::GraphPtr graph);
+        virtual void runBasicOperation(graph::NodePtr node, graph::GraphPtr graph);
 
         /**
          * Runs the control structure; after the execution the next instruction 
          * will be the one following the entire control block.
          */
-        void runControlOperation(graph::NodePtr node, graph::GraphPtr graph);
+        virtual void runControlOperation(graph::NodePtr node, graph::GraphPtr graph);
 
         /**
          * Skips to the complementary structure if exists and then executes it.
          */
-        void runComplementaryBlock(std::string opening, graph::GraphPtr graph);
+        virtual void runComplementaryBlock(std::string opening, graph::GraphPtr graph);
 
-        void runLoop(graph::NodePtr start, graph::GraphPtr graph);
+        virtual void runLoop(graph::NodePtr start, graph::GraphPtr graph);
 
         /**
          * Runs a control block, at the end do not skip to the end of the structure.
          */
-        void runBlock(std::string opening, graph::GraphPtr graph);
+        virtual void runBlock(std::string opening, graph::GraphPtr graph);
 
         /**
          * Skips to the end of the control structure.
          */
-        void skipToEndBlock(std::string control, graph::GraphPtr graph);
+        virtual void skipToEndBlock(std::string control, graph::GraphPtr graph);
 
 
-        void rollbackToStart(graph::NodePtr start, graph::NodePtr current, graph::GraphPtr graph);
-        void rollbackToStart(graph::NodePtr end, graph::GraphPtr graph);
+        virtual void rollbackToStart(graph::NodePtr start, graph::NodePtr current, graph::GraphPtr graph);
+        virtual void rollbackToStart(graph::NodePtr end, graph::GraphPtr graph);
 
     private:
         syntax::SyntaxPtr checker;
