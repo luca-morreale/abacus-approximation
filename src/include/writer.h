538b8610428a35e0a7a18ed4d53cb7c639ccae4a
Luca Morreale <luca.morreale93@gmail.com>
2016-12-08 17:53:20 +0100

    Created static class to write cpp file

---
+++ src/include/writer.h
@@ -0,0 +1,59 @@
+#ifndef WRITER_H
+#define WRITER_H
+
+#include <fstream>
+#include <set>
+
+#include "approximation.h"
+#include "graph.h"
+#include "operation_generator.h"
+#include "syntax.h"
+#include "utility.h"
+
+namespace writer {
+
+
+    class Writer {
+    public:
+        static void writeGraph(graph::GraphPtr graph, std::string filename);
+
+    private:
+        static std::set<std::string> filename_used;
+
+        typedef struct {
+            graph::NodePtr node;
+            graph::GraphPtr graph;
+            std::set<std::string> declaredVariables;
+        } ParsingState;
+
+    protected:
+        static void writeNode(ParsingState &state, std::ofstream &cout);
+        static void writeBasicOperation(ParsingState &state, std::ofstream &cout);
+        static void writeControlOperation(ParsingState &state, std::ofstream &cout);
+
+        static void addVariableType(ParsingState &state, std::ofstream &cout);
+
+        static void prepareFile(std::ofstream &cout, ParsingState &state, graph::GraphPtr graph);
+        static void closeFile(std::ofstream &cout, graph::GraphPtr graph, std::string id);
+
+        static void declareAllOutputVariables(std::ofstream &cout, ParsingState &state, graph::GraphPtr graph);
+        static void declareEdgesVariables(std::ofstream &cout, ParsingState &state, graph::Edges edges, std::string defaultType);
+        static void declareNodesVariables(std::ofstream &cout, ParsingState &state, graph::Nodes nodes, std::string defaultType);
+        static std::string builDeclaration(std::string variable, std::string defaultType);
+
+        static void addOuputPrints(std::ofstream &cout, std::string id);
+
+        
+    private:
+        static bool isAlreadyDeclared(ParsingState &state, std::string variable);
+        static void addDeclaration(ParsingState &state, std::string variable);
+        static std::string getTypeFor(std::string variable, std::string defaultType);
+
+        static std::string generateUnconditionedExpression(std::string out);
+        static std::string generateConditionedExpression(graph::NodePtr node);
+
+    };
+
+}
+
+#endif /* WRITER_H */
---
+++ src/writer.cpp
@@ -0,0 +1,178 @@
+#include "include/writer.h"
+
+
+namespace writer {
+
+    void Writer::writeGraph(graph::GraphPtr graph, std::string filename)
+    {
+        ParsingState state;
+        graph::NodePtr *current = &(state.node);
+        
+        std::ofstream cout(filename + ".cpp");
+
+        prepareFile(cout, state, graph);
+
+        while ((*current = graph->next()) != NULL) {
+            writeNode(state, cout);
+        }
+
+        closeFile(cout, graph, filename);
+    }
+
+    void Writer::prepareFile(std::ofstream &cout, ParsingState &state, graph::GraphPtr graph)
+    {
+        cout << "#include <fstream>" << std::endl;
+        cout << generator::generateIncludes() << std::endl;
+        if (is(graph->getType(), "double")) {
+            cout << "long int mask = 9007199254740991;" << std::endl;
+            cout << "typedef union { double f; long int i;} my_type;" << std::endl;
+        } else if (is(graph->getType(), "float")) {
+            cout << "long int mask = 16777215;" << std::endl;
+            cout << "typedef union { float f; int i;} my_type;" << std::endl;
+        } else {
+            cout << "typedef union { int f; int i;} my_type;" << std::endl;
+        }
+
+        cout << generator::generateFunctions(graph->getType()) << std::endl;
+        cout << "int main(void) {" << std::endl << std::endl;
+
+        declareAllOutputVariables(cout, state, graph);
+    }
+
+    void Writer::declareAllOutputVariables(std::ofstream &cout, ParsingState &state, graph::GraphPtr graph)
+    {
+        declareEdgesVariables(cout, state, graph->getEdges(), graph->getType());
+        declareNodesVariables(cout, state, graph->getNodes(), graph->getType());
+    }
+
+    void Writer::declareEdgesVariables(std::ofstream &cout, ParsingState &state, graph::Edges edges, std::string defaultType)
+    {
+        for (auto it = edges.begin(); it != edges.end(); it++) {
+            std::string variable = it->first;
+            std::string value = it->second;
+            if (!isAlreadyDeclared(state, variable)) {
+                cout << builDeclaration(variable, defaultType) << ";" << std::endl;
+                addDeclaration(state, variable);
+            }
+            cout << variable << " = " << value << ";" <<std::endl;
+        }
+    }
+
+    void Writer::declareNodesVariables(std::ofstream &cout, ParsingState &state, graph::Nodes nodes, std::string defaultType)
+    {
+        for (auto it = nodes.begin(); it != nodes.end(); it++) {
+            std::string variable = (*it)->out;
+            if (!isAlreadyDeclared(state, variable) && !syntax::Syntax::isControlOp(variable)) {
+                cout << builDeclaration(variable, defaultType) << ";" << std::endl;
+                addDeclaration(state, variable);
+            }
+        }
+    }
+
+    bool Writer::isAlreadyDeclared(ParsingState &state, std::string variable)
+    {
+        std::string name = syntax::Syntax::getVariableName(variable);
+        return isIn(state.declaredVariables, name);
+    }
+
+    std::string Writer::builDeclaration(std::string variable, std::string defaultType)
+    {
+        std::string name = syntax::Syntax::getVariableName(variable);
+        std::string type = getTypeFor(variable, defaultType);
+
+        if (syntax::Syntax::isArray(variable)) {
+            name += "[(int)" + name + "Length]";
+        }
+        return type + " " + name;
+    }
+
+    std::string Writer::getTypeFor(std::string variable, std::string defaultType) 
+    {
+        if (approximation::isLoopIterator(variable)) {
+            return "int";
+        }
+        return defaultType;
+    }
+
+    void Writer::addDeclaration(ParsingState &state, std::string variable)
+    {
+        std::string name = syntax::Syntax::getVariableName(variable);
+        state.declaredVariables.insert(name);
+    }
+
+    void Writer::closeFile(std::ofstream &cout, graph::GraphPtr graph, std::string id)
+    {
+        addOuputPrints(cout, id);
+
+        cout << "return 0;" << std::endl;
+        cout << "}" << std::endl;
+        cout.close();
+    }
+
+    void Writer::addOuputPrints(std::ofstream &cout, std::string id)
+    {
+        cout << "std::ofstream output_file(\"" << id <<".txt\");" << std::endl;
+        cout << "for (i=0; i<outputLength; i++){" << std::endl;
+        cout << "output_file << output[i] << std::endl;" << std::endl;
+        cout << "}" << std::endl;
+        cout << "output_file.close();" << std::endl;
+    }
+
+    void Writer::writeNode(ParsingState &state, std::ofstream &cout)
+    {
+        if (syntax::Syntax::isControlOp(state.node->out)) {
+            writeControlOperation(state, cout);
+        } else {
+            writeBasicOperation(state, cout);
+        }
+    }
+
+    void Writer::writeBasicOperation(ParsingState &state, std::ofstream &cout)
+    {   
+        addVariableType(state, cout);
+        std::string statement = generator::generateStatement(state.node);
+        cout << statement << std::endl;
+    }
+
+    void Writer::addVariableType(ParsingState &state, std::ofstream &cout) 
+    {
+        if (syntax::Syntax::isArray(state.node->out)) return;
+
+        std::string outputVariable = syntax::Syntax::getIdentifier(state.node->out, state.graph);
+        if (!isIn(state.declaredVariables, outputVariable)) {
+            state.declaredVariables.insert(outputVariable);
+            cout << state.graph->getType() << " ";
+        }
+    }
+
+    void Writer::writeControlOperation(ParsingState &state, std::ofstream &cout)
+    {
+        std::string line;
+        if(isOpEmpty(state.node)) {
+            line = generateUnconditionedExpression(state.node->out);
+        } else {
+            // generate the control operation, in case of else close automatically curly braches!
+            line = generateConditionedExpression(state.node);
+        }
+        cout << line << std::endl;
+    }
+
+    std::string Writer::generateUnconditionedExpression(std::string out)
+    {
+        std::string line = "}";
+        if (is(out, "_else")) {
+            line += syntax::Syntax::convertControlOperation(out);
+        }
+        return line;
+    }
+
+    std::string Writer::generateConditionedExpression(graph::NodePtr node)
+    {
+            std::string controlOperation = syntax::Syntax::convertControlOperation(node->out);
+            //cout << "// " << controlOperation << std::endl;
+            std::string expression = generator::generateExpression(node);
+            return controlOperation + "(" + expression + ") {"; 
+    }
+
+
+} // namespace executer
